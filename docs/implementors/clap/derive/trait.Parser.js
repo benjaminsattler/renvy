56f91a29e308b0d73f1674bab2247b664bb66233
Benjamin Sattler <bsattler.inbox@gmail.com>
2022-06-26 21:15:42 +0200

    Add rendered rustdoc

---
+++ docs/implementors/clap/derive/trait.Parser.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["renvy"] = [{"text":"impl Parser for <a class=\"struct\" href=\"renvy/struct.Args.html\" title=\"struct renvy::Args\">Args</a>","synthetic":false,"types":["renvy::Args"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ docs/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["renvy"] = {"name":"","files":["io.rs","lib.rs","merge.rs","prelude.rs","serde.rs"]};
+createSourceSidebar();
